85bc91044e5bab9e65303cc380f90abc50e1e615
sabihDev <8203sabi.rahman@gmail.com>
2025-03-17 17:23:33 +0500

    Loader added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Home from "./pages/Home";
 import Settings from "./pages/Settings";
@@ -12,23 +12,35 @@ import Navbar from "./components/Navbar";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 
-function App() {
-  const {authUser, checkAuth} = useAuthStore();
+import { Loader } from "lucide-react";
 
-  useEffect(()=>{
+function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
     checkAuth();
-  },[checkAuth]);
+  }, [checkAuth]);
+
+  console.log(authUser);
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/Settings" element={<Settings />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={ authUser ? <Home /> : <Navigate to="/login"/>} />
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/"/>} />
+        <Route path="/register" element={!authUser ? <Register /> : <Navigate to="/"/>} />
+        <Route path="/chat" element={authUser ? <Chat /> : <Navigate to="/login"/>} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login"/>} />
+        <Route path="/Settings" element={authUser ? <Settings /> : <Navigate to="/login"/>} />
+        <Route path="/dashboard" element={authUser ? <Dashboard /> : <Navigate to="/login"/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
