3e8b934f39961eb5d495e32a5fbc3b8f9da26e7b
sabihDev <8203sabi.rahman@gmail.com>
2025-03-20 03:59:59 +0500

    profile page ui designed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <div className="text-lg font-bold">Chatly</div>
+              <div className="text-lg font-bold">Chatsy</div>
             </Link>
           </div>
 
--- frontend/src/store/useAuthStore.jsx
+++ frontend/src/store/useAuthStore.jsx
@@ -60,4 +60,8 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  updateProfile: async (data) =>{
+    
+  },
 }));
